7305ed48ddf7d0f35f959ed56fd7f543db2e989e
Ram nathawat <nathawatr7@gmail.com>
2023-12-07 11:50:14 +0530

    js done

--- callback.js
+++ callback.js
@@ -3,11 +3,24 @@ const newCookie = {name:"Biscotti Cookies"};
 
 
 // Progression 1: create a function to get all the cookies
+function getAllCookies() {
+  console.log(cookies);
+}
 
   // Progression 2: using setTimeout() 
+  setTimeout(getAllCookies, 1000);
+
 
 //Progression 3: Create a function to creat cookies
-
-
+function createCookie(newCookie, callback) {
+  setTimeout(() => {
+    cookies.push(newCookie);
+    console.log("New cookie created:", newCookie);
+    if (typeof callback === "function") {
+      callback(); 
+    }
+  }, 2000);
+}
 
 // Progression 5: calling function
+createCookie(newCookie, getAllCookies);
--- withoutCallback.js
+++ withoutCallback.js
@@ -4,8 +4,19 @@ const newCookie = {name:"Biscotti Cookies"};
 // Progression 1: create a function to get all the cookies
 
   // Progression 2: using setTimeout() - use 1000 units for time parameter
-
+  function getAllCookies() {
+    setTimeout(() => {
+      console.log("All cookies:", cookies);
+    }, 1000);
+  }
 //Progression 3: Create a function to creat cookies
   // use setTimeout() -- use 2000 units for time parameter
-
+  function createCookie(newCookie) {
+    setTimeout(() => {
+      cookies.push(newCookie);
+      console.log("New cookie created:", newCookie);
+    }, 2000);
+  }
 // Progression 4: calling functions
+getAllCookies();
+createCookie(newCookie);
